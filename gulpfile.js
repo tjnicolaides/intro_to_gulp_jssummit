278f765534b9fef25d0d69a4f0175f491ccb5bc2
TJ Nicolaides <tjnicolaides@gmail.com>
2015-02-08 22:06:48 -0500

    Compile Sass with gulp-ruby-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 
 gulp.task('default', function(){
 	console.log('hello Javascript Summit 2015!');
+});
+
+gulp.task('sass', function () {
+    return gulp.src('scss/main.scss')
+		.pipe(sass())
+        .on('error', function (err) { console.log(err.message); })
+        .pipe(gulp.dest('css'));
 });
